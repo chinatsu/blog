395633adcbb3e2a28cf8d250b8e9add88bd585c3
Kent Daleng <kent.stefan.daleng@nav.no>
2020-04-21 21:16:58 +0200

    Add Google login button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React from "react";
+import { GoogleLogin } from "react-google-login"
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleLogin
+        clientId={'228525192911-jt3a6d9eb96du2m3uj71r2m2hctg6kut.apps.googleusercontent.com'}
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 }
